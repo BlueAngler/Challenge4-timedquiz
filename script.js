26ba391ecffe0af00c00b5193ac3e6719c4a6fcd
BlueAngler <dannyo1028@gmail.com>
2022-08-21 14:20:18 -0500

    added questions array

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var questionEl = document.getElementById('question'); 
+var currentquestion= 0; //counter for the question. keeps track of where we are in the "questions"array. 
 var aEL=document.getElementById('a'); 
 var bEL=document.getElementById('b');
 var cEL=document.getElementById('c');
@@ -9,7 +11,42 @@ var questionscontainerEl=document.getElementById('questionscontainer')
 startbuttonEL.addEventListener('click', startGame)
 
 
+var questions = [//array of questions with answer choices. Put questions and answer choices in an array so we can use a "for loop"
+    {//everyting between each set of curly brackets is one object. "question, answerchoices, and correctanswer" 
+    //are properties of that object. 
+        question:"whats java script", 
+        answerchoices: [
+            "internet language", 
+            "rocket science", 
+            "java", 
+            "i dont know"
+        ], 
+        correctanswer: "a"
+        
+    }, 
+    {
+        question:"whats css", 
+        answerchoices: [
+            "internet language", 
+            "making html pretty", 
+            "java", 
+            "i dont know"
+        ], 
+        correctanswer: "b"
+    }, 
+    {
+        question:"whats my name?", 
+        answerchoices: [
+            "internet language", 
+            "rocket science", 
+            "daniel", 
+            "i dont know"
+        ], 
+        correctanswer:"c"
+    }
 
+    
+]
 
 
 
@@ -20,17 +57,20 @@ startbuttonEL.addEventListener('click', startGame)
 
 function startGame () {
     //checking to see if fn works
-    console.log('hello')
+  
     startbuttonEL.classList.add('hide')
     questionscontainerEl.classList.remove('hide')
+    
    
     
 }
 
 
-
-function Answers() {
-
+//function for displaying questions
+function displayquestions () {
+    //references actual question not just order of question.
+    var activequestion= questions[currentquestion] //this will cycle through actual questions array and their positions using the "currentquestion" indez value
+    questionEl.textContent=activequestion.question
 }
 
 
